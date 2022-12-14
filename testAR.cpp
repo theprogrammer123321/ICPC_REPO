caa6919046bbf082e5208c637c6bb2361080213e
TALHA MUMTAZ <87366886+theprogrammer123321@users.noreply.github.com>
2022-12-14 10:52:49 +0500

    Add files via upload

---
+++ testAR.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int mulsum(vector<int> vt1,vector<int> vt2){
+	int r1=vt1[0].size();
+	int c1r2=vt2.size();
+	int c2=vt2[0].size;
+	vector<vector<int>> res=new int[r1][c2];
+
+	int sum=0;
+	for(int i=0;i<r1;i++){
+		for(int j=0;j<c2;j++){
+			sum=0;
+			for(int k=0;k<c1r2;k++){
+				sum+=vt1[i][k]*vt2[k][j];
+			}
+			res[i][j]=sum;
+		}
+	}
+	sum=0;
+	for(int i=0;i<r1;i++){
+		for(int j=0;j<c2;j++){
+			
+			sum+=res[i][j];
+		}
+	}
+	return sum;	
+}
+
+vector<vector<int>> transpose(vector<vector<int>> mat){
+	int c=mat[0].size();
+	int r=mat.size();
+	vector<vector<int>> temp=new int[c][r];
+
+	for(int i=0;i<c;i++){
+		for(int j=0;j<r;j++){
+			vector[i][j]=mat[j][i];
+		}
+	}
+	return temp;
+}
+int main() {
+
+	// freopen("input.txt","r",stdin);
+	// freopen("output.txt","w",stdout);
+
+	int t;
+	cin>>t;
+	for(int i=0)
+	
+	
+	return 0;
+}
